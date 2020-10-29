9844c7b8443a937824b8d43748f5045a5f5e056c
AcademyGaffney <michaelkgaffney@gmail.com>
2020-10-29 12:59:43 -0500

    3

---
+++ Code/session3/geometry/inheritance.py
@@ -0,0 +1,42 @@
+class Rectangle:
+    def __init__(self, length = 1, width = 1):
+        self.__length = length
+        self.__width = width
+
+    def setLength(self, length):
+        self.__length = length
+
+    def setWidth(self, width):
+        self.__width = width
+
+    def getLength(self):
+        return self.__length
+
+    def getWidth(self):
+        return self.__width
+
+"s".
+class Square(Rectangle):
+    def __init__(self, side):
+        Rectangle.__init__(self, side, side)
+
+    def setSide(self, s):
+        Rectangle.setLength(self, s)
+        Rectangle.setWidth(self, s)
+
+    def getSide(self):
+        return self.getWidth()
+
+    def setLength(self, l):
+        Rectangle.setLength(self, l)
+        Rectangle.setWidth(self, l)
+
+    def setWidth(self, l):
+        Rectangle.setLength(self, l)
+        Rectangle.setWidth(self, l)
+
+sq = Square(3)
+sq.setSide(5)
+sq.setLength(4)
+print(sq.getWidth())
+print(sq.getLength())
